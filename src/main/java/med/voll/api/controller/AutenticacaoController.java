7c397bf205e47322228c50e8b66c4cc51bfc54f5
Samuel <samuel.lorand@hotmail.com>
2025-09-09 21:04:09 -0300

    config

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,18 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao) {
+        return null;
+    }
+}
