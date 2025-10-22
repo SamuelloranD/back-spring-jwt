8dc4291a575462f2513c99c7faf6a30dde16e843
Samuel <samuel.lorand@hotmail.com>
2025-10-21 21:15:19 -0300

    Usuario controller e swagger

---
+++ src/main/java/med/voll/api/domain/usuario/DadosCadastroUsuario.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosCadastroUsuario(
+        @NotBlank(message = "Login é Obrigatório!!!")
+        String login,
+        @NotBlank(message = "Senha é Obrigatória")
+        String senha) {
+}
